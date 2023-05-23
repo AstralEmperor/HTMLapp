8d667f4d5294278fafc58e4b67816ee7fc5d5d8a
AstralEmperor <96700405+AstralEmperor@users.noreply.github.com>
2023-05-23 20:23:54 +0200

    v1.1.21

---
+++ JS/displayInfoZahtevi.js
@@ -0,0 +1,32 @@
+const requestBody = document.querySelector('.zahtevi__body');
+
+    fetch('..JS/zahteviData.json',{
+        method:'GET',
+        headers:{
+            "Accept":"application.json"
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        requestData = data;
+        console.log(requestData)
+    })
+     .catch(error => console.log(error))
+
+function displayData(){
+    const requestRow = document.querySelector('.zahtevi__red');
+
+    const zahteviStatus = document.querySelector('.zahtevi__data--status').value; 
+    const zahteviLokacija = document.querySelector('.zahtevi__data--lokacija').value; 
+    const zahteviOpservator = document.querySelector('.zahtevi__data--opservator').value; 
+    const zahteviVrstaOpser = document.querySelector('.zahtevi__data--vrsta-opservacije').value; 
+    const zahteviDatum1 = document.querySelector('.zahtevi__data--datum-slanja').value; 
+    const zahteviDatum2 = document.querySelector('.zahtevi__data--datum-odgovora').value; 
+    const zahteviOpservacija = document.querySelector('.zahtevi__data--opservacija').value; 
+
+    // for(let i = 0; i < requestData.length;i++){
+    //     console.log(requestData.length)
+    // }
+
+    }
+displayData();
