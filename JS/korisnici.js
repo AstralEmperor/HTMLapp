e2d003ceab0b9f7529dd9f1a37878c14ed3c9d80
AstralEmperor <96700405+AstralEmperor@users.noreply.github.com>
2023-05-29 14:18:59 +0200

    v1.2.1

--- JS/korisnici.js
+++ JS/korisnici.js
@@ -30,13 +30,14 @@ getKorisnici().then(korisnici =>{
     info = '';
     for(korisnik of korisnici){
         info += '<tr>\
-        <td>' + korisnik.name + '</td>\
+        <td class ="korisnik__name">' + korisnik.name + '</td>\
         <td>' + korisnik.email + '</td>\
         <td class="korisnik__allowance">' + korisnik.ogranicenje + '</td>\
         <td class="korisnik__status">' + korisnik.status + '</td>\
         <td><button class="main__korisnici-ActionBtn main__korisnici-Action--EditBtn"><img class="main__korisnici-Action" src="../Images/edit.png" alt="editIcon.png"></button><button class="main__korisnici-ActionBtn"><img class="main__korisnici-Action main__korisnici-Action--delete" src="../Images/delete.png" alt="deleteIcon.png"></button></td>\
         </tr>'
-  
+        
+        sortItems(korisnik);
     }
     tbody.innerHTML = info;
 //changes display(status) based on if the user is online or offline
@@ -75,9 +76,64 @@ getKorisnici().then(korisnici =>{
         updateNumberOfItems();
     });
   }
-//   const thKorisnik = querySelector('.th__korisnik');
-//     thKorisnik.addEventListener('click', e =>{
-//         e.preventDefault();
-//         thKorisnik.sort(korisnik.name)
-//     })
+
 });
+// Item sorter, from W3S guide https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_list_desc
+// function sortItems(){
+    
+//     let switching, i, b, shouldSwitch, switchCount, dir = 0;
+
+//     const list = document.querySelector('.main__searchUl');
+//     switching = true;
+// // Sets sorting direction to ascending
+//     let direction = "asc";
+
+//     while (switching){
+//         switching = false;
+
+//        const b = list.querySelectorAll('.korisnik__name');
+
+//         for ( i = 0; i < b.length - 1; i++) {
+//              //no switching at start:
+//             shouldSwitch = false;
+//             if(direction == "asc"){
+//                 console.log('true');
+//                      /* if next item is alphabetically lower than current item,
+//                       mark as a switch and break the loop: */
+//                 if(b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()){
+//                     console.log('something')
+//                     shouldSwitch = true;
+//                     break;
+//                 }
+//             }else if (direction == "desc"){
+//                 console.log('false');
+//                 if(b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase())
+//                 console.log('something-else')
+//                 shouldSwitch = true;
+//                 break;
+//             }
+//         }
+//     }
+//     if (shouldSwitch){
+//         /* If a switch has been marked, make the switch
+//         and mark that a switch has been done: */
+//         b[i].parentNode.insertBefore(b[i + 1], b[i]);
+//         switching = true;
+//         // Each time a switch is done, increase switchcount by 1:
+//         switchcount ++;
+//     } else {
+//          /* If no switching has been done AND the direction is "asc",
+//          set the direction to "desc" and run the while loop again. */
+//          if(switchCount == 0 && direction == "asc"){
+//              console.log('else')
+//              direction = "desc";
+//              switching = true;
+//          }
+//     }
+// }
+const btnKorisnik = document.getElementById('th__korisnik');
+// sort by name on btn click
+btnKorisnik.addEventListener('click', e => {
+        e.preventDefault();
+         sortItems();
+})
