e1f5a0dc33691192fd7657b5ee728e1e1302b936
AstralEmperor <96700405+AstralEmperor@users.noreply.github.com>
2023-06-09 20:28:27 +0200

    v1.3.2 Attempt to make sorting, doesnt work properly

--- JS/korisnici.js
+++ JS/korisnici.js
@@ -20,15 +20,12 @@ function updateNumberOfItems(){
         }
     }
 }
-
-getKorisnici().then(korisnici =>{
-    korisnici;
-    console.log(korisnici)
-    const tbody = document.querySelector('.korisnici__tbody');
+const tbody = document.querySelector('.korisnici__tbody');
 
     // fills table with data brought from JSON file
+function prototype(korisnici){
     info = '';
-    for(korisnik of korisnici){
+    for(korisnik of korisnici){ 
         info += '<tr>\
         <td class ="korisnik__name">' + korisnik.name + '</td>\
         <td>' + korisnik.email + '</td>\
@@ -36,24 +33,23 @@ getKorisnici().then(korisnici =>{
         <td class="korisnik__status">' + korisnik.status + '</td>\
         <td><button class="main__korisnici-ActionBtn main__korisnici-Action--EditBtn"><img class="main__korisnici-Action" src="../Images/edit.png" alt="editIcon.png"></button><button class="main__korisnici-ActionBtn"><img class="main__korisnici-Action main__korisnici-Action--delete" src="../Images/delete.png" alt="deleteIcon.png"></button></td>\
         </tr>'
-        
-        sortItems(korisnik);
     }
     tbody.innerHTML = info;
-//changes display(status) based on if the user is online or offline
-    const status = document.querySelectorAll('.korisnik__status');
-    for(stat of status){
-        if(stat.innerHTML === "online"){
-            stat.innerHTML ='<div class="main__korisnici--onlineStatus online"></div>'
 
-        }else if(stat.innerHTML === "offline"){
-            stat.innerHTML ='<div class="main__korisnici--onlineStatus offline"></div>'
-
-        }else{
-            console.log('error');
+    //changes display(status) based on if the user is online or offline
+        const status = document.querySelectorAll('.korisnik__status');
+        for(stat of status){
+            if(stat.innerHTML === "online"){
+                stat.innerHTML ='<div class="main__korisnici--onlineStatus online"></div>'
+    
+            }else if(stat.innerHTML === "offline"){
+                stat.innerHTML ='<div class="main__korisnici--onlineStatus offline"></div>'
+    
+            }else{
+                console.log('error');
+            }
         }
-    }
-    //changes display(ogranicenje) based on if the user is limited or not
+            //changes display(ogranicenje) based on if the user is limited or not
     const limitation = document.querySelectorAll('.korisnik__allowance');
     for(limit of limitation){
         if(limit.innerHTML === "Da"){
@@ -65,6 +61,13 @@ getKorisnici().then(korisnici =>{
         console.log('error');
     }
   }
+}
+
+getKorisnici().then(korisnici =>{
+    korisnici;
+    console.log(korisnici)
+ 
+    prototype(korisnici);
   //Adds event listener for DELETE button
   let rows = document.querySelectorAll('tbody tr');
   const deleteBtn = document.querySelectorAll('.main__korisnici-Action--delete');
@@ -76,64 +79,78 @@ getKorisnici().then(korisnici =>{
         updateNumberOfItems();
     });
   }
+  const btnKorisnik = document.getElementById('th__korisnik');
+  // sort by name on btn click
+  btnKorisnik.addEventListener('click', e => {
+      e.preventDefault();
+      korisnici.sort((a,b)=>{
+          if(a.name < b.name){
+            return -1;
+          }
+          else if(b.name < a.name){
+            return -1;
+          }
+          return 0;
+      })
+      prototype(korisnici);
+      updateNumberOfItems();
+  })
 
-});
-// Item sorter, from W3S guide https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_list_desc
-// function sortItems(){
+  const btnEmail = document.getElementById('th__email');
+  btnEmail.addEventListener('click', e => {
+    e.preventDefault();
+    korisnici.sort((a,b)=>{
+    if(a.email < b.email){
+        return -1;
+      }
+      else if(b.email < a.email){
+        return -1;
+      }
+      else{
+        return 0;
+      }
+  })
+  prototype(korisnici);
+  updateNumberOfItems();
+  })
+
+  const btnOgranicenje = document.getElementById('th__ogranicen');
+  btnOgranicenje.addEventListener('click', e => {
+    e.preventDefault();
+    korisnici.sort((a,b)=>{
+    if(a.ogranicenje == 'Da' && b.ogranicenje == 'Ne'){
+        return -1;
+      }
+      else if(b.ogranicenje == 'Da' && a.ogranicenje == 'Ne'){
+        return -1;
+      }
+      else{
+        return 0;
+      }
+      
+  })
+  prototype(korisnici);
+  updateNumberOfItems();
+  })
+
+  const btnStatus = document.getElementById('th__status');
+  btnStatus.addEventListener('click', e => {
+    e.preventDefault();
+    korisnici.sort((a,b)=>{
+    if(a.status == 'offline' && b.status == 'online'){
+        return -1;
+      }
+      else if(b.status == 'offline' && a.status == 'online'){
+        return -1;
+      }
+      else{
+        return 0;
+      }
     
-//     let switching, i, b, shouldSwitch, switchCount, dir = 0;
+  })
+  prototype(korisnici);
+  updateNumberOfItems();
+  })
+});
 
-//     const list = document.querySelector('.main__searchUl');
-//     switching = true;
-// // Sets sorting direction to ascending
-//     let direction = "asc";
 
-//     while (switching){
-//         switching = false;
-
-//        const b = list.querySelectorAll('.korisnik__name');
-
-//         for ( i = 0; i < b.length - 1; i++) {
-//              //no switching at start:
-//             shouldSwitch = false;
-//             if(direction == "asc"){
-//                 console.log('true');
-//                      /* if next item is alphabetically lower than current item,
-//                       mark as a switch and break the loop: */
-//                 if(b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()){
-//                     console.log('something')
-//                     shouldSwitch = true;
-//                     break;
-//                 }
-//             }else if (direction == "desc"){
-//                 console.log('false');
-//                 if(b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase())
-//                 console.log('something-else')
-//                 shouldSwitch = true;
-//                 break;
-//             }
-//         }
-//     }
-//     if (shouldSwitch){
-//         /* If a switch has been marked, make the switch
-//         and mark that a switch has been done: */
-//         b[i].parentNode.insertBefore(b[i + 1], b[i]);
-//         switching = true;
-//         // Each time a switch is done, increase switchcount by 1:
-//         switchcount ++;
-//     } else {
-//          /* If no switching has been done AND the direction is "asc",
-//          set the direction to "desc" and run the while loop again. */
-//          if(switchCount == 0 && direction == "asc"){
-//              console.log('else')
-//              direction = "desc";
-//              switching = true;
-//          }
-//     }
-// }
-const btnKorisnik = document.getElementById('th__korisnik');
-// sort by name on btn click
-btnKorisnik.addEventListener('click', e => {
-        e.preventDefault();
-         sortItems();
-})
